db183f54a485912b5c832d775924b828a1474eb7
howardyangyixuan <howardyangyixuan@gmail.com>
2020-09-15 19:09:11 +0800

    引入inquirer，实现命令行列表交互

--- cli.js
+++ cli.js
@@ -1,25 +1,38 @@
-const api = require('./index')
-const { program } = require('commander');
+const api = require("./index")
+const {program} = require("commander")
 program
-  .command('add')
-  .description('add a task')
+  .command("add")
+  .description("add a task")
   .action((...args) => {
     let words
-    if(args.length>1){
-      words = args[1].join(' ')
-    }else{
-      words=''
+    if (args.length > 1) {
+      words = args[1].join(" ")
+    } else {
+      words = ""
     }
-    api.add(words)
-  });
-// program
-// .command('clear')
-//   .description('clear all tasks')
-//   .action(() => {
-//     api.clear()
-//   });
-if(process.argv.length ===2){
-  api.showAll()
-}else{
-  program.parse(process.argv);
+    api.add(words).then(() => {
+      console.log("添加成功")
+    }, () => {
+      console.log("添加失败")
+    })
+  })
+program
+  .command("clear")
+  .description("clear all tasks")
+  .action(() => {
+      api.clear().then(
+        () => {
+          console.log("清除成功")
+        },
+        () => {
+          console.log("清除失败")
+        }
+      )
+    }
+  )
+
+if (process.argv.length === 2) {
+  void api.showAll()
+} else {
+  program.parse(process.argv)
 }
--- index.js
+++ index.js
@@ -13,12 +13,29 @@ module.exports.add = async (title) => {
   await db.write(list)
 }
 
-module.exports.clear = async ()=>{
+module.exports.clear = async () => {
   await db.write([])
 }
-module.exports.showAll = async ()=>{
+module.exports.showAll = async () => {
   const list = await db.read()
-  list.forEach((item)=>{
-    console.log("任务名："+item.title + " 是否完成: "+item.done)
+  let showList = []
+  list.forEach((item,index)=>{
+    showList.push(`${index} - ${item.title} : ${item.done}`)
   })
+  if(list.length===0) {
+    console.log("还没有任务哦～请添加任务")
+    return
+  }
+  "use strict"
+  let inquirer = require("inquirer")
+  inquirer
+    .prompt({
+      type: "list",
+      name: "index",
+      message: "请选择任务",
+      choices: showList
+    })
+    .then((answer) => {
+      console.log(JSON.stringify(answer, null, "  "))
+    })
 }
